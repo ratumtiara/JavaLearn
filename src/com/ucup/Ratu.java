f8681330f8d6bf0d3eb2756957df96219d200a0b
Ratu Mutiara <rmpapp8@gmail.com>
2021-10-06 21:47:23 +0700

    Initial

---
+++ src/com/ucup/Ratu.java
@@ -0,0 +1,10 @@
+package com.ucup; //foldernya ada di dalem com dalem ucup
+public class Ratu {
+    public static void main(String[]args){
+        // System.out.println("Halo Nama aku Ratu"); //println itu nambahin enter
+        System.out.println(args[0]);
+        System.out.println("Halo Nama aku" + args[0]);
+        //printf Bisa nambahin value yang mau diinput
+    }
+    
+}
